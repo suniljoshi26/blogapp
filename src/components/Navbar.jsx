c5c015e22e2fa2b28ade0906c3c8a2c2c81633f5
Sunil Joshi <joshisunil2601@gmail.com>
2023-06-15 11:42:32 +0530

    navbar componenet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <div className="navbar">
+      {" "}
+      <div className="container">
+        <div className="logo">
+          {" "}
+          <h6>logo </h6>
+        </div>
+
+        <div className="links">
+          <Link className="link">
+            <h6>ART </h6>
+          </Link>
+          <Link className="link">
+            <h6>SCIENCR</h6>
+          </Link>
+          <Link className="link">
+            <h6> TECHNOLOGY </h6>
+          </Link>
+          <Link className="link">
+            <h6>CINEMA </h6>
+          </Link>
+          <Link className="link">
+            <h6>DESIGN </h6>
+          </Link>
+          <Link className="link">
+            <h6>FOOD </h6>
+          </Link>
+          <span>sunil</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
