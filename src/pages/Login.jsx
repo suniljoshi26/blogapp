ce07698ab09d528d119e477f87baeab08cbd8b11
Sunil Joshi <joshisunil2601@gmail.com>
2023-06-26 20:42:12 +0530

    login  with contaxt

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -10,18 +11,16 @@ const Login = () => {
   });
   const [error, setError] = useState();
   const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
-  console.log(inputs);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await axios.post(
-        "http://localhost:8000/api/auth/login",
-        inputs
-      );
+      await login(inputs);
       navigate("/");
     } catch (err) {
       setError(err.response.data);
