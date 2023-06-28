00e4fbee5ebda7cdb3fa439538b986ca3772a8e4
Sunil Joshi <joshisunil2601@gmail.com>
2023-06-28 20:33:32 +0530

    jh

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -31,7 +31,6 @@ const Single = () => {
   return (
     <div className="single">
       <div className="content">
-        {" "}
         <img src={post.img} />
         <div className="user">
           <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
